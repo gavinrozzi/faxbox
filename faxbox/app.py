a32f79ae4a8970e16d4f5bce9d02beee0a56f9d5
Jingming Niu <niu@jingming.ca>
2017-05-21 22:53:11 -0700

    Add email webook

--- faxbox/app.py
+++ faxbox/app.py
@@ -9,24 +9,32 @@ app = Flask(__name__)
 fax_client = FaxClient()
 email_client = EmailClient()
 
+
 @app.route('/')
 def index():
     return 'faxbox', 200
 
-@app.route('/api/register', methods=['POST'])
+
+@app.route('/api/v1/email', methods=['POST'])
+def email():
+    print request.values
+    return '', 202
+
+
+@app.route('/api/v1/register', methods=['POST'])
 def register():
 
     return ''
 
 
-@app.route('/api/receive', methods=['POST'])
-def receive_fax():
+@app.route('/api/v1/receive', methods=['POST'])
+def fax():
     print request.values
     return '', 202
 
 
-@app.route('/api/callback', methods=['GET', 'POST'])
-def status_update():
+@app.route('/api/v1/callback', methods=['GET', 'POST'])
+def callback():
     if 'OriginalMediaUrl' in request.values:
         sender = request.values.get('From')
         mail = Mail(
